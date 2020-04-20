6802e63434bc7763c2fc175000bedefc1fdbad95
lost1deer <lost1deer@163.com>
2020-04-20 17:16:58 +0800

    second commit

---
+++ src/studentManager/Student.java
@@ -0,0 +1,44 @@
+package studentManager;
+
+public class Student {
+	private int ID;
+	private String name;
+    private String birDate;
+    private String gender;
+    
+    public int getID() {
+    	return ID;
+    }
+   
+    public String getName() {
+        return name;
+    }
+    
+    public String getbirDate() {
+    	return birDate;
+    }
+    
+    public String getgender() {
+    	return gender;
+    }
+    
+    public void setID(int a) {
+    	ID=a;
+    }
+    
+    public void setName(String b) {
+        name = b;
+    }
+    
+    public void setbirDate(String c) {
+        birDate = c;
+    }
+    
+    public void setgender(String d) {
+        gender = d;
+    }
+
+  
+     
+   
+}
